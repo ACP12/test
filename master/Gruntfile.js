3744f5207214eef4555a522801f3f5a66b961eb2
Ben Warren <ben.warren@bookatable.com>
2014-07-23 14:21:30 +0100

    added email stuff

---
+++ master/Gruntfile.js
@@ -0,0 +1,139 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    assemble:{
+      options: {
+        layout: 'src/layouts/default.hbs',
+        partials: ['src/partials/*.hbs'],
+        data: ['data/*.{json,yml}'],
+        flatten: true
+      },
+      pages:{
+        files: {
+          'dist/': ['src/pages/*.hbs']
+        }
+      }
+    },
+    clean: {
+      all: ['dist/*.html']
+    },
+    
+            connect: {
+            options: {
+                open: true,
+                hostname: 'localhost',
+                port: 8000,
+                livereload: 35729
+            },
+            dev: {
+                options: {
+                    base: 'dist'
+                }
+            },
+            dist: {
+                options: {
+                    keepalive: true,
+                    livereload: false,
+                    base: 'dist'
+                }
+            }
+        },
+    
+    nodemailer: {
+
+      options: {
+        transport: {
+          type: 'SMTP',
+          options: {
+            service: 'Gmail',
+            auth: {
+              user: 'yuppysoul@gmail.com',
+              pass: 'warren666'
+            }
+          }
+        },
+        message: {
+          subject: 'A test e-mail'
+        },
+        recipients: [
+          {
+            email: 'ben.warren@bookatable.com',
+            name: 'yuppysoul'
+          }
+        ]
+      },
+      external: {
+        src: ['dist/email.html']
+      }
+    },
+    premailer: {
+      simple: {
+        options: {},
+        files: {
+          'dist/email.html': ['dist/index.html']
+        }
+      }
+    },
+    sass: {                              
+      dist: {                            
+        options: {                       
+          style: 'compact'
+        },
+        files: {                         
+          'dist/assets/css/style.css': 'src/assets/scss/style.scss',
+        }
+      }
+    },
+    uncss: {
+      dist: {
+
+        options: {
+          ignore       : [
+            '#backgroundTable', 
+            '.body',
+            '.ExternalClass',
+            '.ExternalClass p',
+            '.ExternalClass span',
+            '.ExternalClass td',
+            '.ExternalClass div',
+            '#outlook a'
+
+          ]
+        },
+        files: {
+          'dist/assets/css/style.css': ['dist/index.html']
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true,
+      },
+      css: {
+        files: 'src/assets/scss/*.scss',
+        tasks: 'sass',
+      },
+      template: {
+        files: 'src/**/*.hbs',
+        tasks: 'assemble'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('assemble');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-nodemailer');
+  grunt.loadNpmTasks('grunt-premailer');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-uncss');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['clean', 'assemble', 'sass', 'connect:dev', 'watch']);
+  grunt.registerTask('premail', ['premailer']);
+  grunt.registerTask('small', ['uncss']);
+  grunt.registerTask('email', ['uncss','premailer','nodemailer']);
+  grunt.registerTask('email2', ['premailer','nodemailer']);
+
+};
---
+++ master/known-issues.md
@@ -0,0 +1,10 @@
+# Known Issues
+
+## Outlook 2007/10/13
+- Weird gap between star deal hero image and next row
+- Weird gap under review stars image (related to hero image gap?)
+- Images with a link (e.g. star deal hero image) become inverted on click.
+- All email text sometimes reverts to the default serif as detailed here: (http://www.emailonacid.com/forum/viewthread/183/#788)
+
+## Outlook.com
+ - Retina images refuse to stay within their containing box only on outlook.com when viewed in Internet Explorer (tested with IE11). This problem does not manifest itself in any other browser.
